79aae9323f6b47b3000eb33329461987c4533e38
nika.soltani <nika.soltani@tapsi.cab>
2022-12-18 10:49:12 +0330

    resolve bug

--- doc_pos.py
+++ doc_pos.py
@@ -1,22 +1,22 @@
 class DocPos:
-    my_map = {}
+    # my_map = {}
 
     # docId = ""
     # positionsList = []
 
     def __init__(self):
         # self.doc_id = doc_id
+        self.my_map = {}
+        self.positions = []
         pass
 
     # def add_doc(self, doc_id):
     #     self.docId = doc_id
 
     def new_doc_id(self, doc_Id, position):
-        positions = []
-        positions.append(position)
-        self.my_map[doc_Id] = positions
-
-
+        # positions = []
+        self.positions.append(position)
+        self.my_map[doc_Id] = self.positions
 
     def add_position(self, doc_id, position):
         # self.positionsList.append(position)
